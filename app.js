f2a31fba5f8e741414fb9ad30cc7b6ae74057eaf
Ajii <ajiedafuq@gmail.com>
2025-05-05 16:50:50 +0700

    fix(device_telemetry) export func

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import startGrpcClient from './client/device_telemetry_grpc.js';
 import { startWebSocketServer } from './server/websocket.js';
+import { startDeviceTelemetryClient } from './client/device_telemetry_grpc.js';
 
 startWebSocketServer();
 startDeviceTelemetryClient();
